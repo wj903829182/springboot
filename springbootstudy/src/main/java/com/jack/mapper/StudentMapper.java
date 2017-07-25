41f36976c8cf9ccfe9d74137827bc11caf43e226
jack <wj903829182@sina.cn>
2017-07-25 10:20:02 +0800

    添加分页插件，集成分页功能

--- springbootstudy/src/main/java/com/jack/mapper/StudentMapper.java
+++ springbootstudy/src/main/java/com/jack/mapper/StudentMapper.java
@@ -3,8 +3,5 @@ package com.jack.mapper;
 import com.jack.entity.Student;
 import tk.mybatis.mapper.common.Mapper;
 
-/**
- * StudentMapper集成了通用Mapper，提供了单表的基础操作
- */
 public interface StudentMapper extends Mapper<Student> {
 }
