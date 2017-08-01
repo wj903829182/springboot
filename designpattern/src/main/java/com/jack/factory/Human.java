2b4544e2f75824731f8f2e8f1d895e0eb30ab90f
jack <wj903829182@sina.cn>
2017-08-01 15:56:31 +0800

    工厂模式

---
+++ designpattern/src/main/java/com/jack/factory/Human.java
@@ -0,0 +1,17 @@
+package com.jack.factory;
+
+/**
+ * Created by jack on 2017/8/1.
+ * 人类接口
+ */
+public interface Human {
+    /**
+     * 每个人种的皮肤都有相应的颜色
+     */
+    void getColor();
+
+    /**
+     * 人会说话交谈
+     */
+    void talk();
+}
