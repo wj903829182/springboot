45161c22152c62ef63ca0110a411b2f07c5d9618
jack <wj903829182@sina.cn>
2017-08-07 15:09:27 +0800

    装饰模式

---
+++ designpattern/src/main/java/com/jack/decoratemodel/FouthGradeSchoolReport.java
@@ -0,0 +1,17 @@
+package com.jack.decoratemodel;
+
+/**
+ * Created by jack on 2017/8/7.
+ * 四年级成绩单
+ */
+public class FouthGradeSchoolReport extends  SchoolReport{
+    @Override
+    public void report() {
+
+    }
+
+    @Override
+    public void sign() {
+
+    }
+}
