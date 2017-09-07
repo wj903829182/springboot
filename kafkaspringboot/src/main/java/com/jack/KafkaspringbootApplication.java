46632e96c3e01e98709bf65aa872c9c4d4ec1edc
jack <wj903829182@sina.cn>
2017-09-07 11:23:31 +0800

    kafka和spring boot

---
+++ kafkaspringboot/src/main/java/com/jack/KafkaspringbootApplication.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaspringbootApplication.class, args);
+	}
+}
