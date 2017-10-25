ce8cef07110cef5e4f2002f2ee97aaea6b1e3fea
jack <wj903829182@sina.cn>
2017-10-25 11:28:27 +0800

    spring boot websocket

---
+++ springboot-websocket-chat/src/main/java/com/jack/SpringbootWebsocketChatApplication.java
@@ -0,0 +1,12 @@
+package com.jack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebsocketChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebsocketChatApplication.class, args);
+	}
+}
