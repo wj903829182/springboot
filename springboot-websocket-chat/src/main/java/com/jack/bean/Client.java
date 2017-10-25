43d41c9a68d380a9a6878d1e4eacdf19b0910b5c
jack <wj903829182@sina.cn>
2017-10-25 17:29:19 +0800

    spring boot websockert另外一种方式实现websocket的客户端

---
+++ springboot-websocket-chat/src/main/java/com/jack/bean/Client.java
@@ -0,0 +1,26 @@
+package com.jack.bean;
+
+import org.springframework.stereotype.Component;
+
+import javax.websocket.*;
+
+/**
+ * Created by jack on 2017/10/25.
+ */
+@ClientEndpoint
+public class Client {
+    @OnOpen
+    public void onOpen(Session session) {
+        System.out.println("Connected to endpoint: " + session.getBasicRemote());
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+        System.out.println(message);
+    }
+
+    @OnError
+    public void onError(Throwable t) {
+        t.printStackTrace();
+    }
+}
---
+++ springboot-websocket-chat/src/main/java/com/jack/test/WebsocketClient2.java
@@ -0,0 +1,36 @@
+package com.jack.test;
+
+/**
+ * Created by jack on 2017/10/25.
+ */
+
+import com.jack.bean.Client;
+
+import javax.websocket.ContainerProvider;
+import javax.websocket.DeploymentException;
+import javax.websocket.Session;
+import javax.websocket.WebSocketContainer;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * http://www.cnblogs.com/akanairen/p/5616351.html
+ */
+public class WebsocketClient2 {
+
+    public static void main(String[] args) {
+        try {
+
+            WebSocketContainer container = ContainerProvider.getWebSocketContainer(); // 获取WebSocket连接器，其中具体实现可以参照websocket-api.jar的源码,Class.forName("org.apache.tomcat.websocket.WsWebSocketContainer");
+            String uri = "ws://localhost:9090/websocket";
+            Session session = null; // 连接会话
+            session = container.connectToServer(Client.class, new URI(uri));
+            session.getBasicRemote().sendText("123132132131"); // 发送文本消息
+            session.getBasicRemote().sendText("4564546");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+}
