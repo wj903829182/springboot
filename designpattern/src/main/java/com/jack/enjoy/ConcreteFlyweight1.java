778c8442d97d3b74873a6645ba71c397a786ff1c
jack <wj903829182@sina.cn>
2018-01-30 14:24:55 +0800

    享元模式

---
+++ designpattern/src/main/java/com/jack/enjoy/ConcreteFlyweight1.java
@@ -0,0 +1,20 @@
+package com.jack.enjoy;
+
+/**
+ * Created by jack on 2018/1/30.
+ * 具体享元角色
+ */
+public class ConcreteFlyweight1 extends Flyweight{
+    // 接受外部状态
+    public ConcreteFlyweight1(String _Extrinsic){
+        super(_Extrinsic);
+    }
+
+    /**
+     * 根据外部状态进行逻辑处理
+     */
+    @Override
+    public void operate() {
+        //业务处理逻辑
+    }
+}
---
+++ designpattern/src/main/java/com/jack/enjoy/Flyweight.java
@@ -0,0 +1,25 @@
+package com.jack.enjoy;
+
+/**
+ * Created by jack on 2018/1/30.
+ * 抽象享元角色
+ */
+public abstract class Flyweight {
+        // 内部状态
+        private String intrinsic;
+        // 外部状态
+        protected final String Extrinsic;
+        // 要求享元角色必须接受外部状态
+        public Flyweight(String _Extrinsic){
+            this.Extrinsic = _Extrinsic;
+        }
+        // 定义业务操作
+        public abstract void operate();
+        // 内部状态的 getter/setter
+        public String getIntrinsic() {
+            return intrinsic;
+        }
+        public void setIntrinsic(String intrinsic) {
+            this.intrinsic = intrinsic;
+        }
+}
